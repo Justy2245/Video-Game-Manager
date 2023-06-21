9565da577f2881664721f8f9fa095e72d40130d9
Justin Takayoshi <takayoshi2245@gmail.com>
2023-06-21 16:17:38 -0700

    Edded Electron to run frontend in an application

--- frontend/src/Components/Layout.js
+++ frontend/src/Components/Layout.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Input from './Input';
-
 const Layout = () => {
     
     const [VGames, setVGames] = useState([]);
@@ -10,11 +9,17 @@ const Layout = () => {
             const data = await fetch('http://localhost:4000/videogames');
             const json = await data.json();
             setVGames(json);
-            console.log(VGames);
         } catch (error) {
             console.log(error.message)
         }
     }
+
+    const execute = (event) => {
+        const { execFile } = require('child_process');
+        const child = execFile('C:/Users/Justin/AppData/Local/Postman/Postman.exe', (error) => {
+            console.log(error);
+        });
+    } 
 
     useEffect(() => {
         getVGames();
@@ -31,6 +36,7 @@ const Layout = () => {
                     <section key ={VGames.vg_id}>
                         <img src={`${VGames.picturelink}`} alt=""></img>
                         <h4>{VGames.name}</h4>
+                        <button onClick = {execute}>Launch Game</button>
                     </section>
                 ))}
             </div>
